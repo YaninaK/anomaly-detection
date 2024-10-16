44e83660bcd6ac028d9f591a566de409bb0ac625
YaninaK <kutovaiayp@yahoo.com>
2024-10-16 10:49:13 +0300

    Add find_missing.py

---
+++ src/anomaly_detection/data/find_missing.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "src", "anomaly_detection"))
+
+
+import logging
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["find_missing_records"]
+
+SAVE = False
+FILE_PATH = "results/missing_data.xlsx"
+
+
+def get_missing_records(
+    data: pd.DataFrame, save: Optional[bool] = None, file_path: Optional[str] = None
+) -> pd.DataFrame:
+    """
+    Выявляет нулевые значения показаний за тепловую энергию в отопительный период (октябрь-апрель)
+    """
+    if save is None:
+        save = SAVE
+    if file_path is None:
+        file_path = FILE_PATH
+
+    cond1 = data["Период потребления"].apply(lambda x: x.month not in range(5, 10))
+    df = data[cond1].pivot_table(
+        index="Адрес объекта",
+        columns="Период потребления",
+        values="Текущее потребление, Гкал",
+    )
+    df.replace(0, np.nan, inplace=True)
+
+    r_records = df.count(axis=1)
+    address_missing_values = r_records[r_records < len(df.columns)].index
+    cond2 = data["Адрес объекта"].isin(address_missing_values)
+
+    df1 = data[cond1 & cond2].groupby(["Период потребления", "Адрес объекта"]).last()
+    df2 = df.loc[address_missing_values].unstack()
+
+    cols = [col for col in data.columns[1:] if col != "Дата текущего показания"]
+    missing_data = (
+        pd.concat([df1, df2], axis=1)
+        .reset_index()[cols]
+        .sort_values(["Адрес объекта", "Период потребления"], ascending=[True, False])
+        .reset_index(drop=True)
+    )[cols]
+
+    for col in [
+        "Подразделение",
+        "№ ОДПУ",
+        "Вид энерг-а ГВС",
+        "Тип объекта",
+        "Адрес объекта 2",
+    ]:
+        missing_data[col] = missing_data.groupby("Адрес объекта")[col].transform(
+            lambda x: x.ffill().bfill()
+        )
+
+    missing_data.loc[
+        missing_data["Текущее потребление, Гкал"] == 0, "Текущее потребление, Гкал"
+    ] = np.nan
+
+    if save:
+        missing_data.iloc[:, :-1].to_excel(file_path)
+
+    return missing_data
