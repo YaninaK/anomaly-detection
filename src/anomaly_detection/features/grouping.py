d6ccd5438988347801a455ffb9b0fb09cc8ab4ba
YaninaK <kutovaiayp@yahoo.com>
2024-10-18 12:51:45 +0300

    Update grouping.py

--- src/anomaly_detection/features/grouping.py
+++ src/anomaly_detection/features/grouping.py
@@ -1,5 +1,6 @@
 import logging
 
+import numpy as np
 import pandas as pd
 
 logger = logging.getLogger(__name__)
@@ -30,6 +31,9 @@ class Grouping:
         """
         Для типа объекта Многоквартирный дом создает группу этажность объекта
         и группу год постройки
+        Добавляет признак Вид энерг-а ГВС и данные по потреблению теплоэнергии многоквартирных
+        домов.
+        Оставляет только объекты с данными по потреблению теплоэнергии.
 
         """
         df = buildings[buildings["Тип Объекта"] == "Многоквартирный дом"].copy()
@@ -49,14 +53,7 @@ class Grouping:
             ],
             labels=self.year_labels,
         )
-        df = df.merge(
-            data[data["Тип объекта"] == "Многоквартирный дом"]
-            .groupby("Адрес объекта 2", as_index=False)["Вид энерг-а ГВС"]
-            .first(),
-            how="right",
-            left_on="Адрес объекта 2",
-            right_on="Адрес объекта 2",
-        )
+        df = self.add_data_info(data, df)
 
         return df
 
@@ -75,4 +72,36 @@ class Grouping:
             "Дата постройки 2"
         ].transform(lambda x: x.fillna(x.median()))
 
+        return df
+
+    def add_data_info(self, data: pd.DataFrame, df: pd.DataFrame) -> pd.DataFrame:
+        """
+        Добавляет признак Вид энерг-а ГВС и данные по потреблению теплоэнергии многоквартирных
+        домов.
+        Оставляет только объекты с данными по потреблению теплоэнергии.
+
+        """
+        cond = data["Тип объекта"] == "Многоквартирный дом"
+        df = df.merge(
+            data[cond]
+            .groupby("Адрес объекта 2", as_index=False)["Вид энерг-а ГВС"]
+            .first(),
+            how="right",
+            left_on="Адрес объекта 2",
+            right_on="Адрес объекта 2",
+        )
+        df = df.merge(
+            data[cond]
+            .pivot_table(
+                index="Адрес объекта 2",
+                columns="Период потребления",
+                values="Текущее потребление, Гкал",
+                dropna=False,
+            )
+            .replace(0, np.nan)
+            .reset_index(),
+            how="left",
+            left_on="Адрес объекта 2",
+            right_on="Адрес объекта 2",
+        )
         return df
