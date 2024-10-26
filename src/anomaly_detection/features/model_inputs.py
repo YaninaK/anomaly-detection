8bd775903383d614428988ab335a4a95b202f58c
YaninaK <kutovaiayp@yahoo.com>
2024-10-26 16:26:46 +0300

    Update model_inputs.py

--- src/anomaly_detection/features/model_inputs.py
+++ src/anomaly_detection/features/model_inputs.py
@@ -1,9 +1,8 @@
 import logging
-from typing import Optional, Tuple
+from typing import Optional
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -11,15 +10,6 @@ __all__ = ["generate_model_inputs"]
 
 
 SEQ_LENGTH = 4
-FEATURES = [
-    "Адрес объекта 2",
-    "№ ОДПУ",
-    "Тип объекта",
-    "Вид энерг-а ГВС",
-    "Группа этажность объекта",
-    "Группа год постройки",
-    "Группа общая площадь объекта",
-]
 
 
 def generate_model_inputs(
@@ -27,30 +17,23 @@ def generate_model_inputs(
     temperature: pd.DataFrame,
     df_stat: pd.DataFrame,
     seq_length: Optional[int] = None,
-    features: Optional[list] = None,
-) -> list[Tuple[list, list, list]]:
-    """
-    Генерирует входные данные для моделей, работающих с последовательностями и
-    статическими признаками.
-    Для каждого объекта выводит:
-    - индекс: ["Адрес объекта 2", "№ ОДПУ"]
-    - статические признаки
-    - последовательность длиною seq_length.
-    """
+) -> pd.DataFrame:
+
     if seq_length is None:
         seq_length = SEQ_LENGTH
-    if features is None:
-        features = FEATURES
 
-    input_list = []
-    for i in tqdm(df_stat.index):
-        sequence_list = generate_sequence_list(i, df_seq, temperature, seq_length)
-        if sequence_list:
-            stat_features = df_stat.loc[i, features].tolist()
-            for sequence in sequence_list:
-                input_list.append((stat_features[:2], stat_features[2:], sequence))
+    df = df_stat.copy()
 
-    return input_list
+    df["seq"] = df.reset_index()["index"].apply(
+        generate_sequence_list,
+        df=df_seq,
+        temperature=temperature,
+        seq_length=seq_length,
+    )
+    df["len_seq"] = df["seq"].apply(lambda x: len(x))
+    df["len_cumsum"] = df["len_seq"].cumsum()
+
+    return df
 
 
 def generate_sequence_list(
@@ -64,7 +47,6 @@ def generate_sequence_list(
     2. Возвращает список последовательностей размерностью seq_length для объекта n.
     """
     x = df.iloc[n, :]
-    t_ = temperature["t_scaled"]
     sequence_list = []
     s = 0
     for ind, num in enumerate(x):
@@ -74,9 +56,13 @@ def generate_sequence_list(
             s += 1
             if s == seq_length:
                 seq = [x[ind - k] for k in reversed(range(seq_length))]
-                temp = [t_[ind - k] for k in reversed(range(seq_length))]
+                temp = [
+                    temperature["t_scaled"][ind - k]
+                    for k in reversed(range(seq_length))
+                ]
+                ozp = [temperature["ОЗП"][ind - k] for k in reversed(range(seq_length))]
 
-                sequence_list.append(list(zip(seq, temp)))
+                sequence_list.append(list(zip(seq, temp, ozp)))
                 s = seq_length - 1
 
     return sequence_list
