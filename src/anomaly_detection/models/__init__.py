98cef923d61dc326592e197440f7cb9f46ca5a0c
YaninaK <kutovaiayp@yahoo.com>
2024-10-29 09:07:25 +0300

    Update __init__.py

--- src/anomaly_detection/models/__init__.py
+++ src/anomaly_detection/models/__init__.py
@@ -9,11 +9,33 @@ AUTOENCODER_CONFIG = {
         "Улица",
         "Вид энерг-а ГВС",
     ],
+    "seq_length": 4,
     "n_features": 3,
     "stat_units_max": 64,
     "stat_units_min": 32,
     "input_sequence_length": 4,
     "output_sequence_length": 4,
+    "n_units_max": 32,
+    "n_units_min": 16,
+}
+
+ETHALON_MODEL_CONFIG = {
+    "features": [
+        "Этажность объекта",
+        "Общая площадь объекта",
+        "Группа общая площадь объекта",
+        "Тип объекта",
+        "Группа этажность объекта",
+        "Группа год постройки",
+        "Улица",
+        "Вид энерг-а ГВС",
+    ],
+    "seq_length": 4,
+    "n_features": 3,
+    "stat_units_max": 64,
+    "stat_units_min": 32,
+    "input_sequence_length": 3,
+    "output_sequence_length": 1,
     "n_units_max": 32,
     "n_units_min": 16,
 }
