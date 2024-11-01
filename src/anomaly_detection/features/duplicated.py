0164ee364bb6acc8c3c7e764a7373ffd61ed44de
YaninaK <kutovaiayp@yahoo.com>
2024-11-01 20:37:11 +0300

    Update duplicated.py

--- src/anomaly_detection/features/duplicated.py
+++ src/anomaly_detection/features/duplicated.py
@@ -1,22 +1,34 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "src", "anomaly_detection"))
+
 import logging
 from typing import Optional
 
 import numpy as np
 import pandas as pd
+from data.preprocess import Preprocess
 
 logger = logging.getLogger(__name__)
 
-__all__ = ["find_equal_values"]
+__all__ = ["equal_values_identification"]
+
 
 SAVE = False
 PATH = ""
-FILE_NAME = "results/equal_values.xlsx"
+FOLDER = "results/2_equal_records/"
+FILE_NAME = "equal_values.xlsx"
 
 
-def get_equal_values(
+def equal_values_identification_pipeline(
     data: pd.DataFrame,
+    buildings: pd.DataFrame,
+    temperature: pd.DataFrame,
     save: Optional[bool] = None,
     path: Optional[str] = None,
+    folder: Optional[str] = None,
     file_name: Optional[str] = None,
 ) -> pd.DataFrame:
     """
@@ -33,8 +45,21 @@ def get_equal_values(
         save = SAVE
     if path is None:
         path = PATH
+    if folder is None:
+        folder = FOLDER
     if file_name is None:
-        file_name = f"{path}{FILE_NAME}"
+        file_name = FILE_NAME
+
+    file_name = f"{path}{folder}{file_name}"
+
+    logging.info("Prefiltering data...")
+
+    preprocess = Preprocess()
+    data, buildings, temperature = preprocess.fit_transform(
+        data, buildings, temperature
+    )
+
+    logging.info("Idenfifying equal values...")
 
     data["Текущее потребление, Гкал"].replace(0, np.nan, inplace=True)
     combined_index = [
@@ -59,6 +84,8 @@ def get_equal_values(
     )
 
     if save:
+        logging.info("Saving dataframes with equal values...")
+
         equal_values.to_excel(file_name, index=False)
 
     return equal_values
